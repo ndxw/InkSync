3ceeea13dafeade200310d92d388d88b5ff1eff5
Zhe(Vincent) Wang <wangz595@mcmaster.ca>
2025-03-24 21:05:41 -0400

    optical sensor done

--- src/main/main.ino
+++ src/main/main.ino
@@ -6,41 +6,123 @@
 #include <BleConnectionStatus.h>
 #include <BleMouse.h>
 
-SPI spi();
+SPI spi;
 BleMouse bleMouse("InkSync", "The InkSync Boys", 100);
 char buffer[64];
+
+int8_t delta_xl; 
+int8_t delta_yl; 
+int8_t delta_xh; 
+int8_t delta_yh; 
+
+// full data
+int16_t delta_x; 
+int16_t delta_y; 
+
+int16_t remaining_x;
+int16_t remaining_y;
+
 
 void setup() {
   Serial.begin(115200);
   spi.init();
 
-  // haven't tested this yet
-  // int status = initOpticalSensor();
-  // if (status == -1)
-  // { 
-  //   Serial.println("Incorrect product ID during initialization!"); 
-  //   ESP.restart(); 
-  // }
-  // else if (status != 0)
-  // { 
-  //   sprintf(buffer, "Initialization failed at step %d", status);
-  //   Serial.println(buffer);
-  //   ESP.restart();
-  // }
+  Serial.println("Process of testing begins"); 
+  int status = initOpticalSensor();
+  Serial.println(status);
+  Serial.println("YESSS"); 
+  if (status == -1)
+  { 
+    Serial.println("Incorrect product ID during initialization!"); 
+    ESP.restart(); 
+  }
+  else if (status != 0)
+  { 
+    sprintf(buffer, "Initialization failed at step %d", status);
+    Serial.println(buffer);
+    ESP.restart();
+  }
 
+  //bluetooth connection
+  bleMouse.begin();
+
+  // while(!bleMouse.isConnected()){
+  //   delay(1000);
+  // }
 }
 
 void loop() {
-
-
-  // use WRITE_PROTECT register to test spi.write()
+  //use WRITE_PROTECT register to test spi.write()
+  Serial.println("Entered"); 
   uint8_t ret = spi.read(WRITE_PROTECT);
   sprintf(buffer, "WP = 0x%X", ret);
   Serial.println(buffer);
 
-  while(1){
-    delay(3000);
-  }  
+  // Serial.println("MOTION_STATUS"); 
+  // Serial.println(spi.read(MOTION_STATUS)); 
+    
+  // read opreation
+  if (((spi.read(MOTION_STATUS)) >> 7) == 1){
+    delta_xl = spi.read(DELTA_XL);
+    delta_yl = spi.read(DELTA_YL);
+    delta_xh = spi.read(DELTA_XH); 
+    delta_yh = spi.read(DELTA_YH); 
+
+    delta_x = (int16_t)(((delta_xh << 8) | (delta_xl & 0xFF)));
+    delta_y = (int16_t)(((delta_yh << 8) | (delta_yl & 0xFF)));
+
+    Serial.println("x movement"); 
+    Serial.println(delta_xl); 
+    Serial.println("y movement"); 
+    Serial.println(delta_yl); 
+
+    remaining_x = delta_x;
+    remaining_y = delta_y;
+  
+  // Loop until both X and Y movements are fully processed.
+  while (remaining_x != 0 || remaining_y != 0) {
+    int8_t stepX, stepY;
+
+    // Break X movement into an 8-bit step.
+    if (remaining_x > 127) {
+      stepX = 127;
+    } else if (remaining_x < -128) {
+      stepX = -128;
+    } else {
+      stepX = (int8_t)remaining_x;
+    }
+
+    // Break Y movement into an 8-bit step.
+    if (remaining_y > 127) {
+      stepY = 127;
+    } else if (remaining_y < -128) {
+      stepY = -128;
+    } else {
+      stepY = (int8_t)remaining_y;
+    }
+
+    // Serial.println("stepX"); 
+    // Serial.println(stepX); 
+    // Serial.println("stepY"); 
+    // Serial.println(stepY); 
+
+    //Send the move command with the computed step values.
+    bleMouse.press(MOUSE_LEFT);
+    bleMouse.move(stepX, stepY, 0, 0);
+
+    // Update the remaining movement.
+    remaining_x -= stepX;
+    remaining_y -= stepY;
+    
+
+    }
+  }
+  else if (((spi.read(MOTION_STATUS)) >> 7) == 0){
+    Serial.println("Thre are no direction detected"); 
+    bleMouse.release(MOUSE_LEFT);
+    
+  }
+    
 }
 
 /*
@@ -54,7 +136,11 @@ Returns:
 */
 int initOpticalSensor()
 {
-  if (spi.read(PRODUCT_ID1) != 0x31){ return -1; }
+  if (spi.read(PRODUCT_ID1) != 0x31){ 
+    Serial.println("Product ID: ");
+    Serial.println(spi.read(0x00)); 
+    return -1; 
+    }
 
   uint8_t initAddresses[] = {0x7F,0x05,0x09,0x51,0x0D,0x0E,0x07,0x1B,0x2E,0x32,0x33,0x34,0x36,0x38,0x39,0x3E,0x44,0x4A,0x4F,0x52,0x57,0x59,0x5B,0x5C,0x7F,0x00,0x07,0x20,0x21,0x23,0x2F,0x37,0x3B,0x43,0x59,0x5A,0x5C,0x5E,0x7F,0x51,0x7F,0x05,0x06,0x07,0x08,0x7F,0x05,0x53,0x7F,0x00,0x09,0x0A,0x0B,0x0C,0x0D,0x12,0x14,0x16,0x18,0x19,0x1A,0x1B,0x20,0x22,0x24,0x26,0x2F,0x30,0x3D,0x3E,0x7F,0x34,0x7F,0x00,0x02,0x03,0x06,0x0F,0x14,0x35,0x36,0x46,0x47,0x4B,0x7F,0x09,0x4B,0x7F,0x09};
   uint8_t initValues[]    = {0x00,0xA8,0x5A,0x06,0x1F,0x1F,0x00,0x42,0x40,0x40,0x02,0x00,0xE0,0xA0,0x01,0x14,0x02,0xE0,0x02,0x0D,0x03,0x03,0x03,0xFF,0x01,0x25,0x78,0x00,0x40,0x00,0x64,0x30,0x64,0x0A,0x01,0x01,0x04,0x04,0x02,0x02,0x03,0x0C,0x0C,0x0C,0x0C,0x04,0x01,0x08,0x05,0x02,0x01,0x1C,0x24,0x1C,0x24,0x08,0x02,0x02,0x1C,0x24,0x1C,0x24,0x08,0x02,0x02,0x88,0x7C,0x07,0x00,0x98,0x06,0x03,0x07,0x01,0xC4,0x13,0x0C,0x0A,0x02,0x39,0x3F,0x03,0x0F,0x97,0x00,0x00,0x97,0x00,0x00};
@@ -72,3 +158,11 @@ int initOpticalSensor()
 
   return 0;
 }
+
+
+int readOpticalSensor(){
+  // reading motion status before prostion register
+
+}
+
+
